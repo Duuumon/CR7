d681e2753039f673f5d438aae26ef40ba444aaeb
Duuumon <nesteov2610@gmail.com>
2025-05-18 16:08:58 +0200

    Update database.js

--- api/orders/database.js
+++ api/orders/database.js
@@ -8,7 +8,7 @@ const app = admin.apps.length
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
         }),
-        databaseURL: `${process.env.FIREBASE_PROJECT_URL}.firebaseio.com`
+        databaseURL: process.env.FIREBASE_PROJECT_URL
     });
 console.log("databaze byla vytvorena");
 
