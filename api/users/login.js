5057fefbc2cab42cef1c6bb7218b7678fccfb65a
Duuumon <nesteov2610@gmail.com>
2025-05-18 17:28:28 +0200

    Update login.js

--- api/users/login.js
+++ api/users/login.js
@@ -1,5 +1,5 @@
 export default function handler (request, response){
-    console.log(request.body);
+    const {jmeno, heslo} = request.body;
     if(jmeno == process.env.ADMIN_USER && heslo == process.env.ADMIN_PASS){
         response.status(200).json({success: true}); // 200 znamnena, ze pozadavek byl uspoesne zpracovan
     }else{
