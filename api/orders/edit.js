c691b4439deff11e1c8d3e13d11d45434dbe20c0
Duuumon <nesteov2610@gmail.com>
2025-05-18 21:53:58 +0200

    Update edit.js\

--- api/orders/edit.js
+++ api/orders/edit.js
@@ -2,12 +2,12 @@ import {getDatabase} from './database.js';
 
 export default async function handler(request, response) { // Definuje a exportuje asynchronní funkci handler, která zpracovává HTTP požadavky.
     const database = getDatabase(); // Získání databáze
-    const {idFirebase, parametr, novaHodnota} = request.body;
+    const {idFirebase, parametry, hodnoty} = request.body;
 
     const odkazRezervace = database.ref('rezervace').child(idFirebase); // Odkaz na konkrétní rezervaci v databázi
 
-    for(let i = 0; i < 2; i++){
-        await odkazRezervace.update({ [parametr[i]]: novaHodnota[i]}); // Nastavení nové hodnoty pro daný parametr
+    for(let i = 0; i < parametry.length; i++){
+        await odkazRezervace.update({ [parametry[i]]: hodnoty[i]}); // Nastavení nové hodnoty pro daný parametr
     }
 
     response.status(200).json({success: true}); // Odeslání úspěšné odpovědi
