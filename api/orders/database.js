6daa93d1fe51fb1164a2c08977d30cdedba6361b
Duuumon <nesteov2610@gmail.com>
2025-05-18 15:53:25 +0200

    Update database.js

--- api/orders/database.js
+++ api/orders/database.js
@@ -1,16 +1,5 @@
 import admin from 'firebase-admin';
 
-// Kontrola, jestli jsou všechny potřebné proměnné prostředí nastavené
-if (!process.env.FIREBASE_PROJECT_ID) {
-    throw new Error('FIREBASE_PROJECT_ID');
-}
-if(!process.env.FIREBASE_PRIVATE_KEY){
-    throw new Error('FIREBASE_PRIVATE_KEY');
-}
-if(!process.env.FIREBASE_CLIENT_EMAIL){
-        throw new Error('FIREBASE_CLIENT_EMAIL');
-}
-console.log(process.env.FIREBASE_PROJECT_ID);
 const app = admin.apps.length
     ? admin.app()
     : admin.initializeApp({
