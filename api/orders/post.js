681a041847ce7f3cab57bb382196d415545d935c
Duuumon <nesteov2610@gmail.com>
2025-05-18 16:35:27 +0200

    Update post.js

--- api/orders/post.js
+++ api/orders/post.js
@@ -3,7 +3,7 @@ import { getDatabase } from './database.js';
 export default async function handler(request, response){// Definuje a exportuje asynchronní funkci handler, která zpracovává HTTP požadavky.
     const { id, cas_od, cas_do, datum, email, jmeno, pocet, telefon} = request.body; // Dostabani dat z těla požadavku
     const database = getDatabase(); // Získání databaze
-    const novyZaznam = database.ref('rezervace').post(); // Vytvoření nového záznamu v databázi
+    const novyZaznam = database.ref('rezervace').push(); // Vytvoření nového záznamu v databázi
     await novyZaznam.set({ // Nastavení hodnot nového záznamu
         cas_do: cas_do, 
         cas_od: cas_od,
